aebc2f58ecdc678f857ac1ae0b3a70d2e7fcab89
AryanPrakash11 <prakasharyan227@gmail.com>
2023-05-23 00:56:56 +0530

    New project

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechSolution</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="hhttps://www.youtube.com/@paveit1475" target={"blank"}>
+            Youtube
+          </a>
+          <a href="https://www.instagram.com/aryan_prakash11/" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/AryanPrakash11" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
